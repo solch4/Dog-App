6cd2ede9c2ac59705cb988842597aa6837afbda0
solch4 <sol.maldonado.0209@gmail.com>
2022-09-07 00:27:24 -0300

    footer added

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../styles/Footer.css'
+
+const Footer = () => {
+  return(
+    <footer className="footer-container">
+      Coded with 🧡 by&nbsp;
+      <a href="https://linktr.ee/solch4" target='_blank' rel="noreferrer">Solcito</a>
+    </footer>
+  )
+}
+
+export default Footer
