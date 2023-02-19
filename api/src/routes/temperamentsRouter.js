c22d8dea86c43003ce4bc3a7b3d463bd0b543735
solch4 <sol.maldonado.0209@gmail.com>
2023-02-19 20:28:49 -0300

    code refactored

--- api/src/routes/temperamentsRouter.js
+++ api/src/routes/temperamentsRouter.js
@@ -8,15 +8,16 @@ const { dataApi } = require('../controllers/controllers.js')
 /* [ ] GET /temperaments:
 Obtener todos los temperamentos posibles
 En una primera instancia deberán obtenerlos desde la API externa y guardarlos en su propia base de datos y luego ya utilizarlos desde allí */
-router.get('/', async (req,res) => {
+router.get('/', async (req, res) => {
   const infoApi = await dataApi()
-  const temperaments = infoApi.map(dog => dog.temperaments).join().split(',')
-  const temperamentsForDB = temperaments.map(e => e.trim())
-  temperamentsForDB.forEach(e => {
-    if(e) {
-      Temperaments.findOrCreate({ //método de sequelize. va a buscar el elemento en la tabla, si no lo encuentra crea la nueva entrada
+  const temperaments = new Set(infoApi.map(dog => dog.temperaments).join().split(',').map(e => e.trim()))
+  const temperamentsForDB = [...temperaments]
+  
+  temperamentsForDB.forEach(async (temperament) => {
+    if (temperament) {
+      await Temperaments.findOrCreate({ //método de sequelize. va a buscar el elemento en la tabla, si no lo encuentra crea la nueva entrada
         where: {
-          name: e
+          name: temperament
         }
       })
     }
